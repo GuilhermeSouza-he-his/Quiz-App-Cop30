7be8f5cc1934c07212e3871b7dbc486edf7e2c8c
Guilherme Pacheco De Souza <guilhermesouza23022006@gmail.com>
2025-08-25 14:17:31 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Quiz-App-Cop30/', // 👈 coloque aqui o nome exato do repo
+  base: "/Quiz-App-Cop30/"   // 👈 nome do seu repo no GitHub
 })
